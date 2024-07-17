423df39ff6a9602ce19a793ee93729c1c6a2d839
NikhilKudva <f20212409@goa.bits-pilani.ac.in>
2024-07-18 01:36:34 +0530

    using index.js from components

--- 06currencyconverter/src/App.jsx
+++ 06currencyconverter/src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import  useCurrencyInfo from './hooks/useCurrencyInfo.js'
-import { InputBox } from './components'
+import {InputBox} from './components/index.js'
 
 function App() {
   const [amount, setAmount] = useState(0)
